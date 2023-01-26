a19fec0c61bb10cd97dfeccc165b74a3d61f269a
Juaned <juanedmram@outlook.com>
2023-01-26 18:17:49 +0000

    all changes

--- application.py
+++ application.py
@@ -8,7 +8,7 @@ application = Flask(__name__)
 def hello():
     """Return a friendly HTTP greeting."""
     print("I am inside hello world")
-    return "Welcome, welcome, to the revolution boy"
+    return "Welcome, welcome, to the revolution beach"
 
 
 @application.route("/echo/<name>")
